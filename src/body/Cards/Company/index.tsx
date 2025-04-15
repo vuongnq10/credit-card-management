fc5279dde944c60289c2db1f3e7d119d2216d2d3
Vuong Nguyen <vuong.qnguyen10@gmail.com>
2025-04-15 17:23:43 +0700

    update

---
+++ src/body/Cards/Company/index.tsx
@@ -0,0 +1,9 @@
+const Index = () => {
+  return (
+    <div>
+      <h1>Company&apos;s Card</h1>
+    </div>
+  );
+};
+
+export default Index;
--- src/body/Cards/index.tsx
+++ src/body/Cards/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 import Button from 'components/Button';
 import Icon from 'components/Icon';
 import styles from './styles.module.css';
 
 const Index: React.FC = () => {
+  const [tab, setTab] = useState('myCards');
   return (
     <div className={styles.container}>
       <h3 className={styles.title}>Available balance</h3>
@@ -12,6 +14,16 @@ const Index: React.FC = () => {
         <span className={styles.currency}>S$</span>
         <span className={styles.amount}>3,000</span>
         <Button icon={<Icon name='add' />} label="New card" />
+      </div>
+      <div className={styles.tabs}>
+        <div
+          className={`${styles.tab} ${tab === 'myCards' && styles.tabActive}`}
+          onClick={() => setTab('myCards')}
+        >My debit cards</div>
+        <div
+          className={`${styles.tab} ${tab === 'companyCard' && styles.tabActive}`}
+          onClick={() => setTab('companyCard')}
+        >All company cards</div>
       </div>
     </div>
   );

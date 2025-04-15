aae0bb97de722830de145a061c9b2ad910779add
Vuong Nguyen <vuong.qnguyen10@gmail.com>
2025-04-15 17:09:42 +0700

    update

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './styles.css';
+
 interface IconProps {
   name: string;
   active?: boolean;
@@ -71,7 +73,7 @@ const icons: IconMap = {
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
       <g transform="translate(-27.3 -0.445)">
         <rect className="a" opacity="0" width="16" height="16" transform="translate(27.3 0.445)" />
-        <path fill="#fff" stroke='#325baf' strokeLinecap='round' strokeLinejoin='round' strokeWidth="0.5px" className="b" d="M7.5,0A7.5,7.5,0,1,0,15,7.5,7.508,7.508,0,0,0,7.5,0Zm3.281,8.125H8.125v2.656a.625.625,0,1,1-1.25,0V8.125H4.219a.625.625,0,1,1,0-1.25H6.875V4.219a.625.625,0,1,1,1.25,0V6.875h2.656a.625.625,0,1,1,0,1.25Zm0,0" transform="translate(27.8 0.945)" />
+        <path fill="#fff" stroke='#325baf' strokeLinecap='round' strokeLinejoin='round' strokeWidth="0.5px" className="b add" d="M7.5,0A7.5,7.5,0,1,0,15,7.5,7.508,7.508,0,0,0,7.5,0Zm3.281,8.125H8.125v2.656a.625.625,0,1,1-1.25,0V8.125H4.219a.625.625,0,1,1,0-1.25H6.875V4.219a.625.625,0,1,1,1.25,0V6.875h2.656a.625.625,0,1,1,0,1.25Zm0,0" transform="translate(27.8 0.945)" />
       </g>
     </svg>
   ),
